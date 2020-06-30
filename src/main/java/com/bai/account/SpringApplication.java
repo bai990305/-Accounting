8131ad95de775f01993cb6a5bccfa2d4aae56b03
bai990305 <990305b@sina.com>
2020-06-30 18:39:24 +0800

    feat(dependency) 1.integration for mybatis and springboot

--- src/main/java/com/bai/account/SpringApplication.java
+++ src/main/java/com/bai/account/SpringApplication.java
@@ -1,4 +1,10 @@
 package com.bai.account;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(SpringApplication.class,args);
+    }
 }
